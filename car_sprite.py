aaa5da6d3944ebbe3e9b2706565d80b89afe46cb
Mikko Mäkelä-Vaitilo <mikko.makela-vaitilo@groke-tech.com>
2024-10-30 11:44:41 +0200

    latest

--- car_sprite.py
+++ car_sprite.py
@@ -9,6 +9,7 @@ object itself, but the car dynamic model object does that.
 import pygame
 import dynamic_car_model as car
 import math
+import numpy as np
 
 
 from pygame.locals import (
@@ -53,41 +54,53 @@ class carSprite(pygame.sprite.Sprite):
         self.positionX = posX
         self.positionY = posY
         self.angle = angle
-        self.cornerpoints = [self.rect.topright, self.rect.topleft, self.rect.bottomleft, self.rect.bottomright]
-        cornerangle = math.atan(self.rect.height/self.rect.width)*180/math.pi
+        self.cornerpoints = [np.array([[self.rect.topright[0]], [self.rect.topright[1]]]), 
+                             np.array([[self.rect.topleft[0]], [self.rect.topleft[1]]]), 
+                             np.array([[self.rect.bottomleft[0]], [self.rect.bottomleft[1]]]), 
+                             np.array([[self.rect.bottomright[0]], [self.rect.bottomright[1]]])]
+                                      
+        # cornerangle = math.atan(self.rect.height/self.rect.width)*180/math.pi
       
-        self.cornerdistance = math.sqrt((self.rect.width/2)*(self.rect.width/2) + (self.rect.height/2)*(self.rect.height/2))
-        self.cornerangles = [cornerangle, 180-cornerangle, 180+cornerangle, -cornerangle]
-        cornerpoints2 = []
-        for corner in self.cornerangles:
-            cornerpointx = self.rect.centerx + self.cornerdistance*math.cos(corner*math.pi/180)
-            cornerpointy = self.rect.centery - self.cornerdistance*math.sin(corner*math.pi/180)
-            cornerpoints2.append([round(cornerpointx), round(cornerpointy)])
-        print (self.cornerpoints)
-        print (cornerpoints2)
-        print("leveys-pituus", self.rect.width, self.rect.height)
+        # self.cornerdistance = math.sqrt((self.rect.width/2)*(self.rect.width/2) + (self.rect.height/2)*(self.rect.height/2))
+        # self.cornerangles = [cornerangle, 180-cornerangle, 180+cornerangle, -cornerangle]
+        # cornerpoints2 = []
+        # for corner in self.cornerangles:
+        #     cornerpointx = self.rect.centerx + self.cornerdistance*math.cos(corner*math.pi/180)
+        #     cornerpointy = self.rect.centery - self.cornerdistance*math.sin(corner*math.pi/180)
+        #     cornerpoints2.append([round(cornerpointx), round(cornerpointy)])
+        # print (self.cornerpoints)
+        # print (cornerpoints2)
+        # print("leveys-pituus", self.rect.width, self.rect.height)
         #self.surf = self.rotate(self.angle)
     
         
-    # Rotation of sprite over a pivot point. Point expressed as offset pixels from center point    
+    # Rotation of sprite over a pivot point. Point expressed as offset pixels from sprite center point    
     def rotate_point(self, rot, offsetX, offsetY):
         
         
         offsetDistance = math.sqrt(offsetX*offsetX + offsetY*offsetY)
         offsetAngle = math.atan2(offsetY, offsetX)*180/math.pi
-    #    print("offsetAngle:", offsetAngle)
         offsetPointOriginalLocationX = offsetDistance*math.sin((self.angle + offsetAngle)*math.pi/180)
         offsetPointOriginalLocationY = offsetDistance*math.cos((self.angle+offsetAngle)*math.pi/180)
-     #   print("alkup. erotus", offsetPointOriginalLocationX, offsetPointOriginalLocationY)
         self.angle += rot
         self.rotate(self.angle)
         offsetPointNewLocationX = offsetDistance*math.sin((self.angle+offsetAngle)*math.pi/180)
         offsetPointNewLocationY = offsetDistance*math.cos((self.angle+offsetAngle)*math.pi/180)
         movementX = offsetPointNewLocationX-offsetPointOriginalLocationX
         movementY = offsetPointNewLocationY-offsetPointOriginalLocationY
-      #  print("movements", movementX, movementY)
-       # print("kulma", self.angle)
         self.rect.move_ip(movementX, movementY)
+        new_cornerpooints = []
+      
+        for cornerpoint in self.cornerpoints:
+            print("täällä")
+            translatedPoint = self.rotate_corners(cornerpoint, self.rect.center)
+            new_cornerpooints.append(translatedPoint)
+            print(translatedPoint)
+        self.cornerpoints = new_cornerpooints
+        print (self.cornerpoints)
+        
+        
+        
         
         
     def accelerate(self, acceleration):
@@ -99,10 +112,25 @@ class carSprite(pygame.sprite.Sprite):
     def brake(self, brakepower):
         self.car.brake(brakepower)
         
-    def update_cornerpoints(self):
-        for element in self.cornerpoints:
-            pass
-        
+    def rotate_corners(self, cornerpoint, rotationpoint):
+        x, y = cornerpoint
+        cx, cy = rotationpoint
+    
+        # Convert angle from degrees to radians
+        angle_rad = np.radians(self.angle)
+    
+        # Create the 2D rotation matrix
+        rotation_matrix = np.array([
+            [np.cos(angle_rad), -np.sin(angle_rad)],
+            [np.sin(angle_rad),  np.cos(angle_rad)]
+            ])
+    
+        # Translate point to origin (center as reference)
+        translated_point = np.array([x - cx, y - cy])
+    
+        # Apply the rotation matrix
+        rotated_point = np.dot(rotation_matrix, translated_point)
+        return rotated_point    
     
     def update(self):
     
@@ -148,14 +176,19 @@ class carSprite(pygame.sprite.Sprite):
         # print("Muutokset", muutosX, muutosY, deltaA)   
         # print("residuaali", self._Xres, self._Yres)
         self.rotate_point(deltaA, -40, 0)
-        index = 0
-        cornerpoint = [0,0]
-        for corner in self.cornerangles:
-            ang = corner + self.angle
-            cornerpoint[X] = self.rect.centerx + self.cornerdistance*math.cos(ang*math.pi/180)
-            cornerpoint[Y] = self.rect.centery - self.cornerdistance*math.sin(ang*math.pi/180)
-            self.cornerpoints[index] = cornerpoint
-       # pygame.draw(self.surf, [0, 255, 0], False, self.cornerpoints)
+        
+        cornerpoint = []
+        self.cornerpoints = []
+     #   print("Kulmat", self.cornerangles)
+     #    for corner in self.cornerangles:
+     #        ang = corner + self.angle
+     #        cornerpoint.append(self.rect.centerx + self.cornerdistance*math.cos(ang*math.pi/180))
+     #        cornerpoint.append(self.rect.centery - self.cornerdistance*math.sin(ang*math.pi/180))
+     #        self.cornerpoints.append(cornerpoint)
+     #    startPos = (self.cornerpoints[TOPLEFT][X], self.cornerpoints[TOPLEFT][Y])
+     #    endPos = (self.cornerpoints[TOPRIGHT][X], self.cornerpoints[TOPRIGHT][Y])
+     #    pygame.draw.line(self.surf, [0, 255, 0], startPos, endPos, 3)
+     # #   print("Viivan pisteet", startPos, endPos)
         self.rect.move_ip(-muutosX, muutosY)
         
         
